d3bcf3e588914aba3af118917f9459ff9581f39b
mozhumz <554501252@qq.com>
2020-08-21 12:52:04 +0800

    git init

---
+++ demoDay21_recsys_music/hyj/config_hyj.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import os
+# 存储一切对应代码的配置信息，主要的配置信息在这个文件中进行修改
+
+# 原始数据路径
+# F:\\idea_workspace\\bigdata\\bigdata_demo_py\\demoDay21_recsys_music\\data
+# 'G:\\idea_workspace\\bigdata\\bigdata_demo_py\\demoDay21_recsys_music\\data\music_data'
+data_path = 'F:\\idea_workspace\\bigdata\\bigdata_demo_py\\demoDay21_recsys_music\\data\\music_data'
+# 中间数据路径
+music_mid_data_path = "../data/music_mid_data_hyj"
+test_path ="%s/test/test.txt" % "../data/music_mid_data_hyj2"
+
+# 读取音乐源数据
+def music_data(nrows=None):
+    music_meta=os.path.join(data_path,'music_meta')
+    data=pd.read_csv(music_meta,nrows=nrows,sep='\001',names=['item_id','item_name','desc','total_timelen','location','tags'],
+                dtype={'item_id':str,'item_name':str,'desc':str,'total_timelen':str,'location':str,'tags':str})
+
+    data.fillna('-')
+    del data['desc']
+    return data
+
+# 读取用户信息
+def user_profile(nrows=None):
+    user_meta=os.path.join(data_path,'user_profile.data')
+    return pd.read_csv(user_meta,nrows=nrows,sep=',',names=['user_id','gender','age','salary','province'],
+                       dtype={'user_id':str,'gender':str,'age':str,'salary':str,'province':str})
+
+# print(user_profile(10))
+
+# 读取用户听过的音乐
+def user_watch(nrows=None):
+    data=os.path.join(data_path,'user_watch_pref.sml')
+    return pd.read_csv(data,nrows=nrows,names=['user_id','item_id','stay_seconds','hour'],sep='\001',
+                       dtype={'user_id':str,'item_id':str,'stay_seconds':int,'hour':int})
+
+# print(user_watch(10))
+
+# #########路径配置#################
+train_file = '%s/train_dict.txt' % music_mid_data_path
+
+# 相似度矩阵存储路径
+user_user_sim_file = '%s/sim_data/uu.sim' % music_mid_data_path
+item_item_sim_file = '%s/sim_data/ii.sim' % music_mid_data_path
+
+# 最终候选集存储
+cf_rec_lst_outfile = '%s/reclst.dict' % music_mid_data_path
+
+# 交叉特征存储
+cross_file = '%s/cross_feat.dict' % music_mid_data_path
+
+# one-hot编码映射表
+user_feat_map_file = '%s/feat/one_hot.dict' % music_mid_data_path
+# model储存
+model_file = '%s/models/lr.model' % music_mid_data_path
+
+res_file='%s/out/res.txt' % music_mid_data_path
+
+# 不同召回策略标识
+UCF_PREFIX = 'UCF_'
+ICF_PREFIX = 'ICF_'
+
+if __name__ == '__main__':
+    x=float('nan')
+    print(type(x))
+    music_df=music_data()
+    print('ok')
+    # row=music_df['item_id'].head()[0]
+    #
+    # print(type(row))
+    # # print(type(row['item_id']))
+    # # music_df['item_id'].astype(str)
+    # music_df2=music_df.loc[music_df['item_id']=='867100256',:]
+    # print(1)
+    # print(music_df2)
---
+++ demoDay22_decisionTree/testHyj.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import math
+import numpy as np
+df = pd.DataFrame({'Animal': ['Falcon', 'Falcon',
+                              'Parrot', 'Parrot'],
+                   'Max Speed': [380., 370., 24., 26.],
+                   'Col_c':['c1','c2','c3','c4']
+                   })
+df1 = pd.DataFrame({'Animal': ['Falcon-df1', 'Falcon-df1',
+                              'Parrot-df1', 'Parrot-df1'],
+                   'id':[1,2,3,4]
+                   })
+df2 = pd.DataFrame({'Animal2': ['Falcon-df2', 'Falcon-df2',
+                               'Parrot-df2', 'Parrot-df2'],
+                    'id':[1,2,3,4]
+                    })
+
+df1['id']=df1['id'].apply(lambda x:str(x)+'_')
+df2['id']=df2['id'].apply(lambda x:str(x)+'_')
+df1=pd.merge(df1,df2,on='id')
+print(df1)
+
+# df1.set_index('id',drop=False,inplace=True)
+# df2.set_index('id',drop=False,inplace=True)
+# print(df1)
+# df1.id=df1.id.astype(np.int32)
+# # df1的id去匹配df2的index
+# df1['Animal2']=df1.id.map(df2.Animal2)
+print(df1.dtypes)
+
+#
+# # print(df)
+#
+# df_g=df.groupby(['Animal']).agg(list).rename(columns={'Max Speed':'Max Speed-arr','Col-c':'Col-c-arr'})
+# print(df_g)
+#
+# a=[1,2]
+# b=[1,2]
+# # 引用比较
+# print(a is b)
+# # 内容比较
+# print(a==b)
+#
+# def getHAD(a_list,total):
+#     res=.0
+#     for i in a_list:
+#         res-=i/total*math.log2(i/total)
+#     return res
+# total=100.
+# a_list=[50,50]
+# print(getHAD(a_list,total))
+# print('-------------------')
+# a_list=[50,40,10]
+# print(getHAD(a_list,total))
+# order_id='1'
+# labels=[]
+# user_products=['1','2','3']
+# train=['2','3']
+# labels += [(order_id, product) in train for product in user_products]
+# print(labels)
+# arr1=np.array(["1","2","3"],dtype=np.str)
+# print(arr1)
+# print('abc'+(arr1[0]))
+
+# print(df)
+# df.set_index('Col_c',drop=False,inplace=True)
+# print(df[0])
+# for idx,row in df.iterrows():
+#     print(row['Col-c'])
+
+# t1=(5,-1,2)
+# t2=(2,1,3)
+# print(max(t1,t2))
+
+# print(df.Animal['c1'])
+
+# df1=pd.DataFrame({'id':[1,2,3],'name':['Andy1','Jacky1','Bruce1']})
+# df2=pd.DataFrame({'id':[1,2],'name':['Andy2','Jacky2']})
+#
+# s = df2.set_index('id')['name']
+# print('df1',df1)
+# print('----------------')
+# print('s',s)
+# print('----------------')
+# # df1['name'] = df1['id'].map(s).fillna(df1['name']).astype(str)
+# print(df1['id'].map(s))
+# print('----------------')
+# print(df1['id'].map(s).fillna(df1['name']).astype(str))
+
+df = pd.DataFrame(np.random.randn(10000, 4))
+
+df.iloc[:9998] = np.nan
+
+sdf = df.astype(pd.SparseDtype("float", np.nan))
+
+sdf.head()
