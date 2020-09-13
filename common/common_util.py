51367b085c3439d0b7321aa5feea320e904a8dc8
mozhumz <554501252@qq.com>
2020-09-13 21:25:51 +0800

    word2vec practice

--- common/common_util.py
+++ common/common_util.py
@@ -2,10 +2,148 @@ import os
 import re
 from string import punctuation
 import math
+from gensim.corpora import WikiCorpus
+import zhconv
+import jieba
+from datetime import datetime
+
+
+
+def get_timestamp(seconds=0.,print_f=True):
+    '''
+    获取当前时间
+    :return: 返回秒值
+    '''
+    #创建一个datetime类对象
+    dt=datetime.now()
+    if print_f:
+        print_time(dt)
+    return dt.timestamp()+seconds
+
+def print_time(dt=datetime.now()):
+    '''
+    打印当前时间
+    :param dt:
+    :return:
+    '''
+    print('时间：' , dt.strftime( '%Y-%m-%d %H:%M:%S %f' ))
+
+def zh_trim_by_file(input_file_path:str, output_file_path:str):
+    '''
+    根据文件路径去除非中文词
+    :param input_file_path:
+    :param output_file_path:
+    :return:
+    '''
+    # input_file_name = 'wiki.cn.simple.separate.txt'
+    # output_file_name = 'wiki.txt'
+    input_file = open(input_file_path, 'r', encoding='utf-8')
+    output_file = open(output_file_path, 'w', encoding='utf-8')
+    print('zh_trim_by_file start...')
+    lines = input_file.readlines()
+    count = 1
+    cn_reg = '^[\u4e00-\u9fa5]+$'
+    for line in lines:
+        line_list = line.split('\n')[0].split(' ')
+        line_list_new = []
+        for word in line_list:
+            if re.search(cn_reg, word):
+                line_list_new.append(word)
+        print(line_list_new)
+        output_file.write(' '.join(line_list_new) + '\n')
+        count += 1
+        if count % 10000 == 0:
+            print('目前已分词%d条数据' % count)
+    print('zh_trim_by_file done！')
+
+
+
+
+def jieba_cut_by_file(input_file_path:str, output_file_path:str):
+    '''
+    根据文件路径进行jieba分词
+    :param input_file_path:
+    :param output_file_path:
+    :return:
+    '''
+    # input_file_path = 'wiki.cn.simple.txt'
+    # output_file_path = 'wiki.cn.simple.separate.txt'
+    input_file = open(input_file_path, 'r', encoding='utf-8')
+    output_file = open(output_file_path, 'w', encoding='utf-8')
+    print('jieba_cut_by_file start...')
+    lines = input_file.readlines()
+    count = 1
+    for line in lines:
+        # jieba分词的结果是一个list，需要拼接，但是jieba把空格回车都当成一个字符处理
+        output_file.write(' '.join(jieba.cut(line.split('\n')[0].replace(' ', ''))) + '\n')
+        count += 1
+        if count % 10000 == 0:
+            print('目前已分词%d条数据' % count)
+    print('jieba_cut_by_file done！')
+
+
+
+
+def tradition2simple(input_file_path:str, output_file_path:str):
+    '''
+    繁体转简体
+    :param input_file_path:
+    :param output_file_path:
+    :return:
+    '''
+    print('tradition2simple start...')
+    # input_file_name = 'wiki.cn.txt'
+    # output_file_name = 'wiki.cn.simple.txt'
+    input_file = open(input_file_path, 'r', encoding='utf-8')
+    output_file = open(output_file_path, 'w', encoding='utf-8')
+    print('read tradition start...')
+    lines = input_file.readlines()
+    print('read tradition done')
+    print('tradition convert start...')
+    count = 1
+    for line in lines:
+        output_file.write(zhconv.convert(line, 'zh-hans'))
+        count += 1
+        if count % 10000 == 0:
+            print('tradition convert changed:%d count' % count)
+    output_file.close()
+    print('tradition2simple done')
+
+
+
+
+def wikixml2txt(input_file_path:str, output_file_path:str):
+    '''
+    将从网络上下载的xml格式的wiki百科训练语料转为txt格式
+    :param input_file_path:
+    :param output_file_path:
+    :return:
+    '''
+    print('wikixml2txt start...')
+    # input_file_name = 'zhwiki-latest-pages-articles.xml.bz2'
+    # output_file_name = 'wiki.cn.txt'
+    print('read start...')
+    input_file = WikiCorpus(input_file_path, lemmatize=False, dictionary={})
+    print('read done')
+    output_file = open(output_file_path, 'w', encoding="utf-8")
+    print('deal start...')
+    count = 0
+    for text in input_file.get_texts():
+        output_file.write(' '.join(text) + '\n')
+        count = count + 1
+        if count % 10000 == 0:
+            print('目前已处理%d条数据' % count)
+    output_file.close()
+    print('wikixml2txt:deal done!')
+
+
 
 # 标点符号集合
-punc = punctuation + r'··.,;\\《》？！“”‘’@#￥%…&×（）——+【】{};；●，。&～、|\s:：'
+punc = punctuation + r'··.,;\\《》？！“”‘’@#￥%…&×（）——+【】{};；●，。&～、|:：'
+# 标点符号集合包括字母
+punc2=punc + r'\sA-Za-z～()（）【】%*#+-\.\\\/:=：__,，。、;；“”""''’‘？?！!<《》>^&{}|=……'
 dicts = {i: '' for i in punc}
+dicts2 = {i: '' for i in punc2}
 
 
 # 根据文件路径（相对或绝对）创建目录
@@ -16,19 +154,23 @@ def mkdirs(file):
         os.makedirs(dir)
 
 
-def trim_with_space_flag(text, space_flag=True):
+def trim_with_space_flag(text, space_flag=False,trim_abc=False):
     """
     去除标点，用空格替换
+    :param trim_abc 是否去除字母 默认否
     """
     if text is None:
         return ''
     replace = " " if space_flag == True else ""
+    if trim_abc:
+        return re.sub(r"[{}]+".format(punc2), '', text)
     return re.sub(r"[{}]+".format(punc), replace, text)
 
 
 def trim_with_str_translate(text):
     """
     去除标点，用python str自带的translate方法
+    :param trim_abc 是否去除字母 默认否
     """
     if text is None:
         return ''
@@ -54,3 +196,6 @@ def convert_softmax_value(v:[]):
     for j in range(len(v)):
             v[j]=math.exp(v[j])/sum
     return v
+
+if __name__ == '__main__':
+    print_time()
---
+++ demoDay25_CNNAndWord2Vec/word2vec_hyj.py
@@ -0,0 +1,122 @@
+import multiprocessing
+from gensim.models import Word2Vec,fasttext
+from gensim.models.word2vec import LineSentence
+from common import common_util
+import logging
+
+def train_word2vec_model(input_file_path:str, model_file_path:str):
+    '''
+    训练模型
+    参数解释：
+    1.sentences：可以是一个List，对于大语料集，建议使用BrownCorpus,Text8Corpus或·ineSentence构建。
+    2.sg： 用于设置训练算法，默认为0，对应CBOW算法；sg=1则采用skip-gram算法。
+    3.size：是指输出的词的向量维数，默认为100。大的size需要更多的训练数据,但是效果会更好. 推荐值为几十到几百。
+    4.window：为训练的窗口大小，8表示每个词考虑前8个词与后8个词（实际代码中还有一个随机选窗口的过程，窗口大小<=5)，默认值为5。
+    5.alpha: 是学习速率
+    6.seed：用于随机数发生器。与初始化词向量有关。
+    7.min_count: 可以对字典做截断. 词频少于min_count次数的单词会被丢弃掉, 默认值为5。
+    8.max_vocab_size: 设置词向量构建期间的RAM限制。如果所有独立单词个数超过这个，则就消除掉其中最不频繁的一个。每一千万个单词需要大约1GB的RAM。设置成None则没有限制。
+    9.sample: 表示 采样的阈值，如果一个词在训练样本中出现的频率越大，那么就越会被采样。默认为1e-3，范围是(0,1e-5)
+    10.workers:参数控制训练的并行数。
+    11.hs: 是否使用HS方法，0表示: Negative Sampling，1表示：Hierarchical Softmax 。默认为0
+    12.negative: 如果>0,则会采用negative samping，用于设置多少个noise words
+    13.cbow_mean: 如果为0，则采用上下文词向量的和，如果为1（default）则采用均值。只有使用CBOW的时候才起作用。
+    14.hashfxn： hash函数来初始化权重。默认使用python的hash函数
+    15.iter： 迭代次数，默认为5。
+    16.trim_rule： 用于设置词汇表的整理规则，指定那些单词要留下，哪些要被删除。可以设置为None（min_count会被使用）或者一个接受()并返回RU·E_DISCARD,uti·s.RU·E_KEEP或者uti·s.RU·E_DEFAU·T的函数。
+    17.sorted_vocab： 如果为1（defau·t），则在分配word index 的时候会先对单词基于频率降序排序。
+    18.batch_words：每一批的传递给线程的单词的数量，默认为10000
+    :param input_file_path:
+    :param model_file_path:
+    :return:
+    '''
+    model = Word2Vec(LineSentence(input_file_path),
+                     size=400,  # 词向量长度为400
+                     window=5,
+                     min_count=5,
+                     # sg=1,
+                     # iter=20,
+                     workers=multiprocessing.cpu_count())
+    print('转换过程结束！')
+    print('开始保存模型...')
+    model.save(model_file_path)
+    print('鲁迅-v:',model['鲁迅'])
+    print('沙瑞金-高育良-sim:',model.wv.similarity('沙瑞金', '高育良'))
+    print('模型保存结束！')
+
+def train_fasttext_model(input_file_path:str, model_file_path:str):
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    model1 = fasttext.FastText(LineSentence(input_file_path),size=400,window=5)
+    model1.save(model_file_path)
+
+
+def load_model(model_path):
+    return Word2Vec.load(model_path)
+def get_different(words:[],model:Word2Vec):
+    '''
+    找出不同类的词，这里给出了人物分类题：
+    print model.wv.doesnt_match(u"沙瑞金 高育良 李达康 刘庆祝".split())
+　　word2vec也完成的很好，输出为"刘庆祝"。
+    :param words:
+    :param model:
+    :return:
+    '''
+    return model.wv.doesnt_match(words)
+
+def get_sim(w1,w2,model:Word2Vec):
+    '''
+    看两个词向量的相近程度
+    :param w1:
+    :param w2:
+    :param model:
+    :return:
+    '''
+    return model.wv.similarity(w1, w2)
+
+def topn(model:Word2Vec,word,n=10):
+    '''
+    找出某一个词向量最相近的词集合
+    :param model:
+    :param word:
+    :param n:
+    :return:
+    '''
+    return model.wv.similar_by_word(word, topn=n)
+
+if __name__ == "__main__":
+    print('主程序开始执行...')
+    seconds=common_util.get_timestamp()
+    file_pre='F:\\八斗学院\\视频\\14期正式课\\00-data\\word2vec\\'
+    ''' 1 xml转txt'''
+    # xml_path=file_pre+'zhwiki-latest-pages-articles.xml.bz2'
+    # txt_file_path = file_pre+'wiki.cn.txt'
+    # # common_util.wikixml2txt(xml_path,txt_file_path)
+    # # print('1-seconds:',common_util.get_timestamp(-seconds))
+    #
+    # ''' 2 txt繁体转简体'''
+    # simple_file_path = file_pre+'wiki.cn.simple.txt'
+    # common_util.tradition2simple(txt_file_path,simple_file_path)
+    # print('2-seconds:',common_util.get_timestamp(-seconds))
+    # #
+    # # ''' 3 jieba分词'''
+    # jieba_file_path =file_pre+ 'wiki.cn.simple.separate.txt'
+    # common_util.jieba_cut_by_file(simple_file_path,jieba_file_path)
+    # print('3-seconds:',common_util.get_timestamp(-seconds))
+    #
+    # ''' 4 去除非中文词'''
+    wiki_file_path =file_pre+ 'wiki.txt'
+    # common_util.zh_trim_by_file(jieba_file_path,wiki_file_path)
+    # print('4-seconds:',common_util.get_timestamp(-seconds))
+    #
+    # ''' 5 开始训练'''
+    input_file_path =wiki_file_path
+    model_file_path = file_pre+'wiki.model'
+
+    # print('转换过程开始...')
+    # train_word2vec_model(input_file_path,model_file_path)
+    # print('5-seconds:',common_util.get_timestamp(-seconds))
+    model=load_model(model_file_path)
+    print(model.accuracy(questions='F:\\八斗学院\视频\\14期正式课\\00-data\\word2vec\\qu.txt'))
+    # print(topn(model,'鲁迅'))
+
+    print('主程序执行结束！')
