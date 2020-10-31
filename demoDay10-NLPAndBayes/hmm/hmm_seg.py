6e21b2da8052a2a31f75d5d695d07ba294ce0472
mozhumz <554501252@qq.com>
2020-10-31 09:27:14 +0800

    HMM

--- demoDay10-NLPAndBayes/hmm/hmm_seg.py
+++ demoDay10-NLPAndBayes/hmm/hmm_seg.py
@@ -1,9 +1,10 @@
 import jieba
-pre='G:\\bigdata\\badou\\00-data\\'
-mode_path =pre+ 'out\\hmm.mod'
+
+pre = 'G:\\bigdata\\badou\\00-data\\'
+mode_path = pre + 'out\\hmm.mod'
 filter_val = 0.02
 # 1.加载模型
-with open(mode_path,'r',encoding='utf-8') as f:
+with open(mode_path, 'r', encoding='utf-8') as f:
     # 初始状态概率
     pi = eval(f.readline())
     # 状态转移概率
@@ -23,7 +24,7 @@ def get_word_ch(word):
     return ch_lst
 
 
-def viterbi(sentence,sep=' '):
+def viterbi(sentence, sep=' '):
     ch_lst = get_word_ch(sentence)
     # print(ch_lst)
     # 文本序列的长度T
@@ -32,42 +33,42 @@ def viterbi(sentence,sep=' '):
     # 初始化状态矩阵
     # 三维数组 最外层为行状态M（这里共四个状态：BMES），每行是单字符串数组，
     # 数组每个元素有2个属性，第一个为发射概率*初始状态概率 第二个为状态的下标
-    status_matrix = [[[0.0,0] for col in range(ch_num)] for row in range(STATUS_NUM)]
+    status_matrix = [[[0.0, 0] for col in range(ch_num)] for row in range(STATUS_NUM)]
 
     # 状态种类M个  m*1
     for i in range(STATUS_NUM):
         # 第一个字的发射概率
-        if B[i].get(ch_lst[0],-1) != -1:
+        if B[i].get(ch_lst[0], -1) != -1:
             cur_B = B[i][ch_lst[0]]
         else:
             cur_B = -100000.0
 
-        #初始状态概率
+        # 初始状态概率
         # pi_k=p(s1)
         cur_pi = pi[i]
         status_matrix[i][0][0] = cur_pi + cur_B  # 取了log相加就是相乘
         status_matrix[i][0][1] = i
 
     # M*M*(T-1)
-    for i in range(1,ch_num): # 1:T  (T-1)
+    for i in range(1, ch_num):  # 1:T  (T-1)
         for j in range(STATUS_NUM):  # M
             max_p = None
             max_status = None
             # 经过j节点的前M个节点中存储概率最大值，及上一层过来到这个节点的状态编号
-            for k in range(STATUS_NUM): # M 前一层的第k个s
+            for k in range(STATUS_NUM):  # M 前一层的第k个s
                 # k到j的转移概率
                 cur_A = A[k][j]
-                cur_p = status_matrix[k][i-1][0]+cur_A
+                cur_p = status_matrix[k][i - 1][0] + cur_A
                 if max_p is None or max_p < cur_p:
                     max_p = cur_p
                     max_status = k
 
             # 当前字符的发射概率
-            if B[j].get(ch_lst[i],-1)!=-1:
+            if B[j].get(ch_lst[i], -1) != -1:
                 cur_B = B[j][ch_lst[i]]
             else:
                 cur_B = -100000.0
-            status_matrix[j][i][0] = max_p+cur_B
+            status_matrix[j][i][0] = max_p + cur_B
             status_matrix[j][i][1] = max_status
 
     # get max prob path
@@ -79,36 +80,35 @@ def viterbi(sentence,sep=' '):
     #         max_end_p = status_matrix[i][ch_num-1][0]
     #         max_end_status = i
     for i in range(STATUS_NUM):
-        last_prob_lst.append((status_matrix[i][ch_num-1][0],status_matrix[i][ch_num-1][1],i))
-    sorted_last_lst = sorted(last_prob_lst, key=lambda x:x[0], reverse=True)
+        last_prob_lst.append((status_matrix[i][ch_num - 1][0], status_matrix[i][ch_num - 1][1], i))
+    sorted_last_lst = sorted(last_prob_lst, key=lambda x: x[0], reverse=True)
     print(sorted_last_lst)
-
 
     def get_best_seg(max_end_status):
         # 根据之前记录的往回找最优路径
         best_status_lst = [0 for ch in range(ch_num)]
         best_status_lst[ch_num - 1] = max_end_status
 
-        c = ch_num-1
+        c = ch_num - 1
         cur_best_status = max_end_status
         while c > 0:
             # 上一个最好的状态
             pre_best_status = status_matrix[cur_best_status][c][1]
-            best_status_lst[c-1] = pre_best_status
+            best_status_lst[c - 1] = pre_best_status
             cur_best_status = pre_best_status
             c -= 1
 
         # 实现切词
         rest = ''
-        rest +=ch_lst[0]
-        for i in range(1,ch_num):
+        rest += ch_lst[0]
+        for i in range(1, ch_num):
             '''
             0 1 2 3
             B M E S
             '''
             # i-1 是E,S 或者 i是B,S
             # {2,3}表示set集合
-            if best_status_lst[i-1] in {2,3} or best_status_lst[i] in {0,3}:
+            if best_status_lst[i - 1] in {2, 3} or best_status_lst[i] in {0, 3}:
                 rest += sep
             rest += ch_lst[i]
         return rest
@@ -128,7 +128,7 @@ def viterbi(sentence,sep=' '):
 if __name__ == '__main__':
     s = '得到概率最大的状态组合方式'
     s1 = '结巴分词里会结合词性，状态会更多'
-    s2='无边落木萧萧下'
-    print(viterbi(s2,' '))
-    jieba_str=[' '.join([w for w in jieba.cut(s2)])]
+    s2 = '无边落木萧萧下'
+    print(viterbi(s2, ' '))
+    jieba_str = [' '.join([w for w in jieba.cut(s2)])]
     print(jieba_str)
