581197ed5bab5fc27156a12c2508cd78e7f5ef65
mozhumz <554501252@qq.com>
2021-02-02 20:57:33 +0800

    线程池测试

--- test/multiThreadTest.py
+++ test/multiThreadTest.py
@@ -4,32 +4,37 @@
 from concurrent.futures import ThreadPoolExecutor
 import time
 import threading
+import logging
 
 lockVal= []
 global lockVal
 lock=threading.RLock()
 
-def test(value1='v1', value2='v2'):
+def test(value1='v1', value2='v2',log=None):
     print("%s threading is printed %s, %s"%(threading.current_thread().name, value1, value2))
     time.sleep(2)
-    lockAndAdd()
+    lockAndAdd(log)
     return 'finished'
 
 def test_result(future):
     print(future.result())
 
-def lockAndAdd():
+def lockAndAdd(log=None):
     lock.acquire(timeout=5000)
     try:
         lockVal.append(1)
+        log.info('lockAndAdd:'+threading.current_thread().name)
     finally:
         lock.release()
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    logger = logging.getLogger(__name__)
 
+    logger.info('start')
     threadPool = ThreadPoolExecutor(max_workers=4)
     for i in range(0,10):
-        future = threadPool.submit(test)
+        future = threadPool.submit(test,log=logger)
 
     threadPool.shutdown(wait=True)
     print(lockVal)
