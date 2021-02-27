8fd47ddd4a4a830ab7dc7f2b6a6231e07dc150fb
mozhumz <554501252@qq.com>
2021-02-27 13:26:36 +0800

    seq2seq 模型加载报错解决

--- seq2seq_learn/seq2seq.py
+++ seq2seq_learn/seq2seq.py
@@ -7,6 +7,8 @@ from keras.layers import Input
 from keras.layers import LSTM
 from keras.layers import Dense
 import numpy as np
+import os
+import joblib
 # import tensorflow as tf
 # tf.debugging.set_log_device_placement(True)
 
@@ -60,80 +62,99 @@ def define_models(n_input, n_output, n_units):
     decoder_states = [state_h, state_c]
     decoder_outputs = decoder_dense(decoder_outputs)
     decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
+
     # 返回需要的三个模型
     return model, encoder_model, decoder_model
 
-def predict_sequence(infenc, infdec, source, n_steps, cardinality):
-    # 输入序列编码得到编码状态向量
-    state = infenc.predict(source)
-    # 初始目标序列输入：通过开始字符计算目标序列第一个字符，这里是0
-    target_seq = array([0.0 for _ in range(cardinality)]).reshape(1, 1, cardinality)
-    # 输出序列列表
-    output = list()
-    for t in range(n_steps):
-        # predict next char
-        yhat, h, c = infdec.predict([target_seq] + state)
-        # 截取输出序列，取后三个
-        output.append(yhat[0,0,:])
-        # 更新状态
-        state = [h, c]
-        # 更新目标序列(用于下一个词预测的输入)
-        target_seq = yhat
-    return array(output)
+class SeqModel:
+    def __init__(self,n_input, n_output, n_units):
+        self.train_model,self.infenc,self.infdec=define_models(n_input, n_output, n_units)
+        print('init done')
 
-# one_hot解码
-def one_hot_decode(encoded_seq):
-    return [argmax(vector) for vector in encoded_seq]
+    def predict_sequence(self,source, n_steps, cardinality):
+        infenc, infdec=self.infenc,self.infdec
+        # 输入序列编码得到编码状态向量
+        state = infenc.predict(source)
+        # 初始目标序列输入：通过开始字符计算目标序列第一个字符，这里是0
+        target_seq = array([0.0 for _ in range(cardinality)]).reshape(1, 1, cardinality)
+        # 输出序列列表
+        output = list()
+        for t in range(n_steps):
+            # predict next char
+            yhat, h, c = infdec.predict([target_seq] + state)
+            # 截取输出序列，取后三个
+            output.append(yhat[0,0,:])
+            # 更新状态
+            state = [h, c]
+            # 更新目标序列(用于下一个词预测的输入)
+            target_seq = yhat
+        return array(output)
+
+    # one_hot解码
+    def one_hot_decode(self,encoded_seq):
+        return [argmax(vector) for vector in encoded_seq]
 
 # 参数设置
 n_features = 10 + 1
 n_steps_in = 6
 n_steps_out = 3
 # 定义模型
-train, infenc, infdec = define_models(n_features, n_features, 16)
-train.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+pre='models/'
+encoder_file=pre+'encoder.h5'
+decoder_file=pre+'decoder.h5'
+model_file=pre+'model.h5'
+seq_model_file=pre+'seq_model.joblib'
+seq_model=SeqModel(n_features,n_features,16)
+if os.path.exists(encoder_file) and os.path.exists(decoder_file) and os.path.exists(model_file):
+    # train=load_model(model_file)
+    seq_model.infenc.load_weights(encoder_file)
+    seq_model.infdec.load_weights(decoder_file)
+    seq_model.train_model.load_weights(model_file)
+# train, infenc, infdec = seq_model.define_models(n_features, n_features, 16)
+seq_model.train_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 # 生成训练数据
 import pickle
 X1, X2, y = get_dataset(n_steps_in, n_steps_out, n_features, 1000)
 print(X1.shape,X2.shape,y.shape)
 
 # 训练模型
-train.fit([X1, X2], y, epochs=100)
+seq_model.train_model.fit([X1, X2], y, epochs=100)
 
-pre='models/'
-encoder_file=pre+'encoder.h5'
-decoder_file=pre+'decoder.h5'
-model_file=pre+'model.h5'
-def predict(infenc,infdec):
-    import os
-    if os.path.exists(encoder_file) and os.path.exists(decoder_file):
-        # train=load_model(model_file)
-        infenc=load_model(encoder_file)
-        '''
-         Layer lstm_2 expects 1 inputs, but it received 3 input tensors. 
-        '''
-        infdec=load_model(decoder_file)
+
+def predict():
+    global seq_model
+
+    # if os.path.exists(seq_model_file):
+    #     seq_model=joblib.load(seq_model_file)
+
+    '''
+     Layer lstm_2 expects 1 inputs, but it received 3 input tensors.
+    '''
+        # infdec=load_model(decoder_file)
     global _, X1, X2, y, target
     total, correct = 100, 0
     for _ in range(total):
         X1, X2, y = get_dataset(n_steps_in, n_steps_out, n_features, 1)
-        target = predict_sequence(infenc, infdec, X1, n_steps_out, n_features)
-        if array_equal(one_hot_decode(y[0]), one_hot_decode(target)):
+        target = seq_model.predict_sequence( X1, n_steps_out, n_features)
+        if array_equal(seq_model.one_hot_decode(y[0]), seq_model.one_hot_decode(target)):
             correct += 1
     print('Accuracy: %.2f%%' % (float(correct) / float(total) * 100.0))
 
-
 # 评估模型效果
-predict(infenc,infdec)
+predict()
 # 查看预测结果
 for _ in range(10):
     X1, X2, y = get_dataset(n_steps_in, n_steps_out, n_features, 1)
-    target = predict_sequence(infenc, infdec, X1, n_steps_out, n_features)
-    print('X=%s y=%s, yhat=%s' % (one_hot_decode(X1[0]), one_hot_decode(y[0]), one_hot_decode(target)))
+    target = seq_model.predict_sequence( X1, n_steps_out, n_features)
+    print('X=%s y=%s, yhat=%s' % (seq_model.one_hot_decode(X1[0]), seq_model.one_hot_decode(y[0]), seq_model.one_hot_decode(target)))
 
 
+
+seq_model.infenc.save_weights(encoder_file)
+seq_model.infdec.save_weights(decoder_file)
+seq_model.train_model.save_weights(model_file)
+# train.save(model_file)
+
+
+# joblib.dump(seq_model,seq_model_file)
 print('done')
-infenc.save(encoder_file)
-infdec.save(decoder_file)
-train.save(model_file)
-
